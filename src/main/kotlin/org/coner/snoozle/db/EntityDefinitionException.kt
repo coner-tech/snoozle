ce9bf528a8ab326d2ea47acee3c51d860ee1b63f
Carlton Whitehead <cebesius86@gmail.com>
2018-11-25 12:15:58 -0500

    Test pathfinder, improve error handling (#1)
    * Initial tests for Pathfinder
    
    * Expand and test entity definition correctness checks
    
    * Update dependencies
    
    * Describe the EntityDefinitionException
    
    * Test pathfinder finds entities by property arguments
    
    * Test pathfinder finds entity paths by instance
    
    * Test pathfinder finds entity path listings
    
    * Alter entity property type resolution affected by deprecation
    
    * Error handling for pathfinder finding entity from incorrect properties
    
    * Additional error handling for pathfinder find entity from wrong properties
    
    * Error handling for pathfinder find listing from wrong properties

--- src/main/kotlin/org/coner/snoozle/db/EntityDefinitionException.kt
+++ src/main/kotlin/org/coner/snoozle/db/EntityDefinitionException.kt
@@ -1,6 +1,15 @@
 package org.coner.snoozle.db
 
+/**
+ * This exception indicates a snoozle database entity is defined incorrectly.
+ *
+ * Its message should give some context
+ *
+ * As long as developers are constructing database instances with a static
+ * list of Entity types and performing the barest-minimum testing before
+ * shipping, end-users should never experience this.
+ */
 class EntityDefinitionException(
         message: String,
         cause: Throwable? = null
-) : Exception(message, cause)
+) : RuntimeException(message, cause)
--- src/main/kotlin/org/coner/snoozle/db/Pathfinder.kt
+++ src/main/kotlin/org/coner/snoozle/db/Pathfinder.kt
@@ -1,25 +1,32 @@
 package org.coner.snoozle.db
 
-import java.lang.Exception
 import java.util.*
 import kotlin.reflect.KClass
 import kotlin.reflect.KProperty1
 import kotlin.reflect.full.declaredMemberProperties
 import kotlin.reflect.full.findAnnotation
+import kotlin.reflect.full.starProjectedType
+import kotlin.reflect.jvm.jvmErasure
 
 class Pathfinder<E : Entity>(
         val kclass: KClass<E>
 ) {
-    private val entityPathFormat: String
-    private val entityPathReplaceProperties: List<KProperty1<E, UUID>>
-    private val listingPathFormat: String
-    private val listingPathReplaceProperties: List<KProperty1<E, UUID>>
+    val entityPathFormat: String
+    val entityPathReplaceProperties: List<KProperty1<E, UUID>>
+    val listingPathFormat: String
+    val listingPathReplaceProperties: List<KProperty1<E, UUID>>
 
     init {
         fun buildPathReplacementProperties(pathReplacementFormat: String): List<KProperty1<E, UUID>> {
             val openBrackets = pathReplacementFormat.count { it == '{' }
             val closeBrackets = pathReplacementFormat.count { it == '}' }
-            // TODO: handling for malformed entity path value
+            if (openBrackets != closeBrackets) {
+                throw EntityDefinitionException("""
+                    ${kclass.qualifiedName} has a malformed EntityPath: $pathReplacementFormat.
+
+                    There are $openBrackets open bracket(s) and $closeBrackets close bracket(s).
+                """.trimIndent())
+            }
             val entityPathReplacements = openBrackets
             val entityPathReplacementProperties = mutableListOf<KProperty1<E, UUID>>()
             var openBracketPosition: Int
@@ -31,17 +38,33 @@ class Pathfinder<E : Entity>(
                         openBracketPosition + 1,
                         closeBracketPosition
                 )
-                entityPathReplacementProperties.add(
-                        kclass.declaredMemberProperties
-                                .first { it.name ==  entityPathReplacementPropertyName} as KProperty1<E, UUID>
-                )
+                val property = kclass.declaredMemberProperties
+                        .firstOrNull { it.name ==  entityPathReplacementPropertyName } as KProperty1<E, UUID>?
+                if (property != null) {
+                    if (property.returnType != UUID::class.starProjectedType) {
+                        throw EntityDefinitionException("""
+                            ${kclass.qualifiedName} has an invalid EntityPath: $pathReplacementFormat.
+
+                            References property with unexpected type: ${property.returnType.jvmErasure.qualifiedName}.
+
+                            Only ${UUID::class.qualifiedName} is supported.
+                        """.trimIndent())
+                    }
+                } else {
+                    throw EntityDefinitionException("""
+                        ${kclass.qualifiedName} has a malformed EntityPath: $pathReplacementFormat.
+
+                        No such property: $entityPathReplacementPropertyName
+                    """.trimIndent())
+                }
+                entityPathReplacementProperties.add(property)
             }
             return entityPathReplacementProperties.toList()
         }
 
         val entityPath: EntityPath = kclass.findAnnotation()
             ?: throw EntityDefinitionException("""
-                ${kclass.qualifiedName} is not annotated with ${EntityPath::class.qualifiedName}
+                ${kclass.qualifiedName} lacks ${EntityPath::class.qualifiedName} annotation
             """.trimIndent())
         entityPathFormat = entityPath.value
         entityPathReplaceProperties = buildPathReplacementProperties(entityPathFormat)
@@ -50,6 +73,7 @@ class Pathfinder<E : Entity>(
     }
 
     fun findEntity(vararg ids: Pair<KProperty1<E, UUID>, UUID>): String {
+        enforcePropertyArgumentsMatch(ids, entityPathReplaceProperties, entityPathFormat)
         var path = entityPathFormat
         for (id in ids) {
             path = path.replace("{${id.first.name}}", id.second.toString())
@@ -66,6 +90,7 @@ class Pathfinder<E : Entity>(
     }
 
     fun findListing(vararg ids: Pair<KProperty1<E, UUID>, UUID>): String {
+        enforcePropertyArgumentsMatch(ids, listingPathReplaceProperties, listingPathFormat)
         var path = listingPathFormat
         if (ids.isNotEmpty()) {
             for (id in ids) {
@@ -73,5 +98,32 @@ class Pathfinder<E : Entity>(
             }
         }
         return path
+    }
+
+    private fun enforcePropertyArgumentsMatch(
+            ids: Array<out Pair<KProperty1<E, UUID>, UUID>>,
+            replacementProperties: List<KProperty1<E, UUID>>,
+            replacementFormat: String
+    ) {
+        // check counts match
+        if (replacementProperties.size != ids.size) throw IllegalArgumentException("""
+            The passed ids (${ids.joinToString(", ") { it.first.name }})
+            differ from the expected length: ${replacementProperties.size}.
+
+            ${kclass.qualifiedName} requires the following properties:
+            ${replacementProperties.joinToString(", ") { it.name }}
+        """.trimIndent())
+
+        // check all passed properties match
+        if (!ids.map { it.first }.containsAll(replacementProperties)) throw IllegalArgumentException("""
+            The passed id properties do not contain the expected properties referenced by the format:
+
+            $replacementFormat
+
+            Verify the arguments passed only contain properties referenced by the above format.
+
+            Expected: ${replacementProperties.joinToString(", ") { it.name }}
+            Actual: ${ids.joinToString(", ") { it.first.name }}
+        """.trimIndent())
     }
 }
